950ff89537c14bf3eb1a4dab6e4847da34fd96cd
adhamhaddad <adhamhaddad.dev@gmail.com>
2023-11-18 07:40:20 +0200

    Add app and database configs

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import databaseConfig from './config/database.config';
+import appConfig from './config/app.config';
+import config from './config/config';
+import { TypeOrmConfigService } from './database/typeorm-config.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [databaseConfig, appConfig, config],
+      envFilePath: ['.env'],
+    }),
+    TypeOrmModule.forRootAsync({
+      useClass: TypeOrmConfigService,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
